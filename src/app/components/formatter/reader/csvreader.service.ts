8219cb176d1b578a9f1cfb55533f1940b35b5fce
dmcfarl <doctordan81@gmail.com>
2022-05-02 22:17:40 -0400

    Use CSV Streaming for better memory footprint.

--- src/app/components/formatter/reader/csvreader.service.ts
+++ src/app/components/formatter/reader/csvreader.service.ts
@@ -1,4 +1,4 @@
-import { parse, ParseConfig, ParseError, ParseResult } from 'papaparse';
+import { parse, ParseConfig, ParseError, Parser, ParseResult } from 'papaparse';
 import { Column } from './column';
 import { CSVData } from './csvdata';
 import { Injectable } from '@angular/core';
@@ -12,7 +12,7 @@ export class CSVReaderService {
         let promise = new Promise<CSVData>((resolve, reject) => {
             let data = new CSVData();
             data.filename = file.name;
-            const config: ParseConfig = {
+            /*const config: ParseConfig = {
                 header: true,
                 dynamicTyping: true,
                 //preview: 20, // For Testing
@@ -50,6 +50,48 @@ export class CSVReaderService {
                             headerCounts.set(header, 1);
                         }
                     });
+
+                    return dataChunk;
+                }
+            };*/
+            data.parsed = [];
+            const config: ParseConfig = {
+                header: true,
+                dynamicTyping: true,
+                //preview: 20, // For Testing
+                worker: false, // async? may need to keep this false to force synchronous; Doesn't seem to work with beforeFirstChunk
+                skipEmptyLines: true,
+                step: (results: ParseResult<any>, parser: Parser) => {
+                    data.parsed.push(results.data);
+                },
+                complete: () => {
+                    console.log("Parsing complete");
+                    resolve(data);
+                },
+                error: (error: ParseError, file: File) => {
+                    console.log("Error:", error);
+                    reject(error);
+                },
+                beforeFirstChunk: (chunk: string): string => {
+                    let dataChunk = this.ignoreProlog(chunk).replace("Time (s)", "UTC Time (s)").replace("Speed (m/s)", "GPS Speed (m/s)");
+
+                    let headers = dataChunk.slice(0, dataChunk.indexOf('\n')).trim().split(",");
+                    let headerCounts = new Map<string, number>();
+
+                    // Replace any duplicates
+                    headers.forEach((header: string) => {
+                        if (headerCounts.has(header)) {
+                            headerCounts.set(header, headerCounts.get(header) + 1);
+                            dataChunk = this.replaceSecond(dataChunk, header + ",", header + " (" + headerCounts.get(header) + "),");
+                        } else {
+                            headerCounts.set(header, 1);
+                        }
+                    });
+                    if (headers.findIndex(header => header.startsWith("Manifold pressure")) >= 0 && headers.findIndex(header => header.startsWith("Barometric pressure")) >= 0) {
+                        headers.push("Boost (kPa) *OBD");
+                    }
+                    headers.unshift("Time (s)");
+                    data.columns = headers.map(header => new Column(header));
 
                     return dataChunk;
                 }
